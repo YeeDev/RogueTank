a51dccc64b4991affb6b9e4dd3716d5ade5ee5ba
Yee <yee.gamedev@gmail.com>
2024-04-17 13:17:17 -0600

    Mover Refactored

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,40 +1,52 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Yee.Math;
 
-public class Mover : MonoBehaviour
+namespace RTank.Movement
 {
-    [Range(1, 10)] [SerializeField] float speed;
-    [Range(1, 10)] [SerializeField] float rotationSpeed;
-
-    public IEnumerator Move(Vector3 addedPoint)
+    public class Mover : MonoBehaviour
     {
-        Vector3 newPosition = transform.position + addedPoint;
-        Vector3 direction = newPosition - transform.position;
+        [Range(1, 10)] [SerializeField] float speed;
+        [Range(1, 10)] [SerializeField] float rotationSpeed;
 
-        Quaternion lookAt = Quaternion.LookRotation(direction, transform.up);
-
-        float angle = MathY.CalculateFlatAngle(transform.forward, direction);
-        if (angle > 1)
+        public IEnumerator MoveAndRotate(Vector3 addedPoint)
         {
-            float time = 0;
-            float timeToAdd = rotationSpeed * Time.fixedDeltaTime;
-            while (time < 1f)
+            Vector3 movePoint = transform.position + addedPoint;
+            Vector3 direction = movePoint - transform.position;
+
+            float angle = MathY.CalculateFlatAngle(transform.forward, direction);
+            if (angle > 1) { yield return Rotate(angle, direction); }
+
+            yield return Move(movePoint, direction);
+        }
+
+        public IEnumerator Rotate(float angle, Vector3 direction)
+        {
+            Quaternion lookAt = Quaternion.LookRotation(direction, transform.up);
+
+            if (angle > 1)
             {
-                time += angle > 91 ? timeToAdd * 0.5f : timeToAdd;
-                transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, time);
+                float time = 0;
+                float timeToAdd = rotationSpeed * Time.fixedDeltaTime;
+                while (time < 1f)
+                {
+                    time += angle > 91 ? timeToAdd * 0.5f : timeToAdd;
+                    transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, time);
+                    yield return new WaitForFixedUpdate();
+                }
+                transform.rotation = lookAt;
+            }
+        }
+
+        public IEnumerator Move(Vector3 movePoint, Vector3 direction)
+        {
+            while (Vector3.SqrMagnitude(transform.position - movePoint) > 0.01f)
+            {
+                transform.Translate(direction * speed * Time.fixedDeltaTime, Space.World);
                 yield return new WaitForFixedUpdate();
             }
-            transform.rotation = lookAt;
-        }
 
-        while (Vector3.SqrMagnitude(transform.position - newPosition) > 0.01f)
-        {
-            transform.Translate(direction * speed * Time.fixedDeltaTime, Space.World);
-            yield return new WaitForFixedUpdate();
+            transform.position = movePoint;
         }
-
-        transform.position = newPosition;
     }
 }
