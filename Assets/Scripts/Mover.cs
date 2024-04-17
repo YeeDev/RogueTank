a2a88a3e011fbb6d12f511d959c4a0091dd06c78
Yee <yee.gamedev@gmail.com>
2024-04-17 13:02:54 -0600

    Mover Script Written

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yee.Math;
+
+public class Mover : MonoBehaviour
+{
+    [Range(1, 10)] [SerializeField] float speed;
+    [Range(1, 10)] [SerializeField] float rotationSpeed;
+
+    public IEnumerator Move(Vector3 addedPoint)
+    {
+        Vector3 newPosition = transform.position + addedPoint;
+        Vector3 direction = newPosition - transform.position;
+
+        Quaternion lookAt = Quaternion.LookRotation(direction, transform.up);
+
+        float angle = MathY.CalculateFlatAngle(transform.forward, direction);
+        if (angle > 1)
+        {
+            float time = 0;
+            float timeToAdd = rotationSpeed * Time.fixedDeltaTime;
+            while (time < 1f)
+            {
+                time += angle > 91 ? timeToAdd * 0.5f : timeToAdd;
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, time);
+                yield return new WaitForFixedUpdate();
+            }
+            transform.rotation = lookAt;
+        }
+
+        while (Vector3.SqrMagnitude(transform.position - newPosition) > 0.01f)
+        {
+            transform.Translate(direction * speed * Time.fixedDeltaTime, Space.World);
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.position = newPosition;
+    }
+}
