dc24bfab085c28e8b550a674e380740df7ee6df2
Yee <yee.gamedev@gmail.com>
2024-04-17 13:03:05 -0600

    Controller Script Written

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTank.Controls
+{
+    [RequireComponent(typeof(Mover))]
+    public class Controller : MonoBehaviour
+    {
+        bool inAction;
+        Mover mover;
+
+        private void Awake()
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            if (inAction) { return; }
+
+            Vector3 newPosition = Vector3.zero;
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                newPosition = Vector3.right * Input.GetAxisRaw("Horizontal");
+                StartCoroutine(mover.Move(newPosition));
+            }
+            else if (Input.GetButtonDown("Vertical"))
+            {
+                newPosition = Vector3.forward * Input.GetAxisRaw("Vertical");
+                StartCoroutine(mover.Move(newPosition));
+            }
+        }
+    }
+}
