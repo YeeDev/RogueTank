041b4d9b2ec5076100ce8306edbdb9fed8d7b09c
Yee <yee.gamedev@gmail.com>
2024-04-18 13:41:59 -0600

    Added Functionality to Math Library

--- Assets/Scripts/Libraries/MathY.cs
+++ Assets/Scripts/Libraries/MathY.cs
@@ -4,6 +4,23 @@ namespace Yee.Math
 {
     public class MathY : MonoBehaviour
     {
+        public static bool CheckEqualAxis(Vector3 v1, Vector3 v2)
+        {
+            bool xEquals = Mathf.RoundToInt(v1.x) == Mathf.RoundToInt(v2.x);
+            bool zEquals = Mathf.RoundToInt(v1.z) == Mathf.RoundToInt(v2.z);
+
+            return xEquals || zEquals;
+        }
+
+        public static bool CheckEqualAxisY(Vector3 v1, Vector3 v2)
+        {
+            bool xEquals = Mathf.RoundToInt(v1.x) == Mathf.RoundToInt(v2.x);
+            bool yEquals = Mathf.RoundToInt(v1.y) == Mathf.RoundToInt(v2.y);
+            bool zEquals = Mathf.RoundToInt(v1.z) == Mathf.RoundToInt(v2.z);
+
+            return xEquals || yEquals || zEquals;
+        }
+
         public static float CalculateFlatAngle(Vector3 v1, Vector3 v2)
         {
             float angle = Vector3.Angle(FlatY(v1), FlatY(v2));
