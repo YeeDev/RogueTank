bab31196f29b5a74e29556834abf129ecb08ebd1
Yee <yee.gamedev@gmail.com>
2024-04-17 18:09:48 -0600

    Turn Organizer Script Written

---
+++ Assets/Scripts/TurnOrganizer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;
+
+namespace RTank.Core
+{
+    public class TurnOrganizer : MonoBehaviour
+    {
+        public Action OnPlayerEnd;
+
+        bool turnRunning;
+
+        public bool TurnRunning => turnRunning;
+
+        public void RunTurn() => turnRunning = !turnRunning;
+
+        public void EndTurn()
+        {
+            if (OnPlayerEnd != null) { OnPlayerEnd(); }
+            else
+            {
+                //TODO change to actual logic when enemies get implemented
+                turnRunning = false;
+                Debug.Log("Player wins");
+            }
+        }
+    }
+}
