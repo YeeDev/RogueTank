dc9b3ddba53e1acd99a19c4b1275c78c390658c5
Yee <yee.gamedev@gmail.com>
2024-04-18 13:08:33 -0600

    Heavy Refactor

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using RTank.Core;
 using RTank.Movement;
@@ -12,6 +11,7 @@ namespace RTank.Controls
     public class EnemyController : MonoBehaviour
     {
         [SerializeField] LayerMask playerLayer;
+        [SerializeField] float checkerRadius;
 
         Mover mover;
         Shooter shooter;
@@ -35,50 +35,26 @@ namespace RTank.Controls
 
         private void TakeTurn()
         {
-            if (!shooter.HasShell)
-            {
-                StartCoroutine(CallReload());
-            }
-            else if (CheckIfPlayerInRange())
-            {
-                StartCoroutine(CallShoot());
-            }
-            else
-            { 
-                StartCoroutine(CallMove());
-            }
+            if (!shooter.HasShell) { StartCoroutine(CallAction(shooter.Reload())); }
+            else if (CheckIfPlayerInRange()) { StartCoroutine(CallAction(shooter.Shoot())); }
+            else { StartCoroutine(CallAction(mover.MoveAndRotate(Vector3.forward))); }
+        }
+
+        private IEnumerator CallAction(IEnumerator action)
+        {
+            yield return action;
+
+            turnOrganizer.EndEnemyTurn();
         }
 
         private bool CheckIfPlayerInRange()
         {
-            return Physics.Raycast(transform.position + Vector3.forward, transform.forward, 1f, playerLayer);          
-        }
-
-        private IEnumerator CallReload()
-        {
-            yield return shooter.Reload();
-
-            turnOrganizer.EndEnemyTurn();
-        }
-
-        private IEnumerator CallMove()
-        {
-            yield return mover.MoveAndRotate(Vector3.forward);
-
-            turnOrganizer.EndEnemyTurn();
-        }
-
-        private IEnumerator CallShoot()
-        {
-            yield return shooter.Shoot();
-
-            turnOrganizer.EndEnemyTurn();
+            return Physics.Raycast(transform.position + Vector3.forward, Vector3.forward, 1f, playerLayer);
         }
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawRay(transform.position + Vector3.forward, transform.forward);
+            
         }
     }
 }
