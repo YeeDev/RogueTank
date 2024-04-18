4e37c261a4da471c47febb8c385a0d5bb645be68
Yee <yee.gamedev@gmail.com>
2024-04-17 20:26:23 -0600

    Shooter Script Written

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] Transform muzzle;
+    [SerializeField] GameObject shellPrefab;
+
+    public IEnumerator Shoot()
+    {
+        GameObject shell = Instantiate(shellPrefab, muzzle.position, muzzle.rotation);
+
+        yield return new WaitUntil(() => shell == null);
+    }
+}
