201a3f5502652628fbb965ccd8399f77f434753f
Yee <yee.gamedev@gmail.com>
2024-04-17 20:25:11 -0600

    Shell script written

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace RTank.Movement
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Shell : MonoBehaviour
+    {
+        [SerializeField] float speed;
+
+        Rigidbody rb;
+
+        private void Awake() => rb = GetComponent<Rigidbody>();
+
+        private void Start() => rb.velocity = transform.forward * speed * Time.fixedDeltaTime;
+    }
+}
