b079bbb19a06764098b9f2776d3b89fc9b38342b
Yee <yee.gamedev@gmail.com>
2024-04-18 18:37:31 -0600

    Map Data Script Written

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace RTank.CoreData
+{
+    [CreateAssetMenu(menuName = "Map Data", fileName = "New Map Data")]
+    public class MapData : ScriptableObject
+    {
+        [SerializeField] int rows;
+        [SerializeField] int columns;
+
+        public int Rows => rows;
+        public int Columns => columns;
+        public float MidRow => (rows - 1) * 0.5f;
+        public float MidColumn => (columns - 1) * 0.5f;
+    }
+}
