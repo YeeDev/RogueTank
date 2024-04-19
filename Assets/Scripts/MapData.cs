72da385f0d9c3b68be74dd52631e901ca3b45fa9
Yee <yee.gamedev@gmail.com>
2024-04-19 13:01:12 -0600

    Map Data Added functionality

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -8,9 +8,24 @@ namespace RTank.CoreData
         [SerializeField] int rows;
         [SerializeField] int columns;
 
+        long obstaclePositions;
+
         public int Rows => rows;
         public int Columns => columns;
+        public int TotalTiles => rows * columns;
         public float MidRow => (rows - 1) * 0.5f;
         public float MidColumn => (columns - 1) * 0.5f;
+
+        public void SetObstaclePositions(long l) => obstaclePositions = l;
+
+        public int GetFlatArray(int x, int z) => x * columns + z;
+
+        public Vector3 GetCoordinate(int index, float yPosition)
+        {
+            int row = index / columns;
+            int column = index % columns;
+
+            return new Vector3(row, yPosition, column);
+        }
     }
 }
