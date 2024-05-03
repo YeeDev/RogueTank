895a9bdf9db92c1e9f7a4748286dcc38791eac91
Yee <yee.gamedev@gmail.com>
2024-05-02 19:06:49 -0600

    Mist Creator script written

---
+++ Assets/Scripts/Core_scr/MistCreator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using RTank.CoreData;
+
+namespace RTank.Core
+{
+    public class MistCreator : MonoBehaviour
+    {
+        [SerializeField] GameObject mistPrefab;
+
+        public void AddMist(MapData mapData)
+        {
+            for (int x = 0; x < mapData.Columns; x++)
+            {
+                for (int z = 0; z < mapData.Rows; z++)
+                {
+                    Instantiate(mistPrefab, new Vector3(x, 0, z), Quaternion.identity, transform);
+                }
+            }
+        }
+    }
+}
