285ae857038e725430eef40c21ee1567873c0493
Yee <yee.gamedev@gmail.com>
2024-05-02 19:14:54 -0600

    Folder Organization

---
+++ Assets/Scripts/Environment_scr/Revealer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTank.Environment
+{
+    public class Revealer : MonoBehaviour
+    {
+        [SerializeField] string mistTag;
+
+        Transform player;
+        Dictionary<Collider, Animator> mist = new Dictionary<Collider, Animator>();
+
+        private void Awake()
+        {
+            player = transform.root;
+            transform.parent = null;
+        }
+
+        private void LateUpdate() { if (player != null) { transform.position = player.position; } }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(mistTag)) { return; }
+
+            if (!mist.ContainsKey(other)) { mist.Add(other, other.GetComponent<Animator>()); }
+
+            mist[other].SetBool("FadeOut", true);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag(mistTag)) { return; }
+
+            mist[other].SetBool("FadeOut", false);
+        }
+    }
+}
