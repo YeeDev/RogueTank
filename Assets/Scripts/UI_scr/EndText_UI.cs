7de2b6d57350b0a024bff96a50ec3bed6a06b856
Yee <yee.devindie@gmail.com>
2024-07-05 12:06:18 -0600

    UI for winning and losing

---
+++ Assets/Scripts/UI_scr/EndText_UI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using RTank.Core;
+
+namespace RTank.UI
+{
+    public class EndText_UI : MonoBehaviour
+    {
+        [SerializeField] GameObject endPanel;
+        [SerializeField] TextMeshProUGUI endText;
+
+        TurnOrganizer turnOrganizer;
+
+        private void Awake() => turnOrganizer = GameObject.FindGameObjectWithTag("TurnOrganizer").GetComponent<TurnOrganizer>();
+
+        private void OnEnable() => turnOrganizer.OnEndMatch += ActivateWinPanel;
+        private void OnDisable() => turnOrganizer.OnEndMatch += ActivateWinPanel;
+
+        private void ActivateWinPanel(bool playerWon)
+        {
+            endText.text = playerWon ? "You won!" : "You lost!";
+            endPanel.SetActive(true);
+        }
+    }
+}
