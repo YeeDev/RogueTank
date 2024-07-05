ca15182854ca18ede4f563c416fd6f4518437a3d
Yee <yee.devindie@gmail.com>
2024-07-05 12:04:08 -0600

    Game shows whos turn is it

---
+++ Assets/Scripts/UI_scr/Turn_UI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+using RTank.Core;
+
+namespace RTank.UI
+{
+    public class Turn_UI : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI turnText;
+
+        TurnOrganizer turnOrganizer;
+
+        private void Awake() => turnOrganizer = GameObject.FindGameObjectWithTag("TurnOrganizer").GetComponent<TurnOrganizer>();
+
+        private void OnEnable()
+        {
+            turnOrganizer.OnPlayerEnd += SetEnemyTurnText;
+            turnOrganizer.OnEnemyEnd += SetPlayerTurnText;
+        }
+
+        private void OnDisable()
+        {
+            turnOrganizer.OnPlayerEnd -= SetEnemyTurnText;
+            turnOrganizer.OnEnemyEnd -= SetPlayerTurnText;
+        }
+
+        private void SetPlayerTurnText() => turnText.text = "Player";
+        private void SetEnemyTurnText() => turnText.text = "Enemy";
+    }
+}
