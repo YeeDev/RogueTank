365d3ba4be0f06c4fa86dad3f24ae14d6197f361
Yee <yee.devindie@gmail.com>
2024-08-20 20:46:58 -0600

    Player uses tank model and there's a win delay.

--- Assets/Scripts/UI_scr/EndText_UI.cs
+++ Assets/Scripts/UI_scr/EndText_UI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using TMPro;
 using RTank.Core;
+using System.Collections;
 
 namespace RTank.UI
 {
     public class EndText_UI : MonoBehaviour
     {
+        [Range(0, 100)][SerializeField] float waitEndTime = 1;
         [SerializeField] GameObject endPanel;
         [SerializeField] TextMeshProUGUI endText;
 
@@ -13,11 +15,15 @@ namespace RTank.UI
 
         private void Awake() => turnOrganizer = GameObject.FindGameObjectWithTag("TurnOrganizer").GetComponent<TurnOrganizer>();
 
-        private void OnEnable() => turnOrganizer.OnEndMatch += ActivateWinPanel;
-        private void OnDisable() => turnOrganizer.OnEndMatch += ActivateWinPanel;
+        private void OnEnable() => turnOrganizer.OnEndMatch += StartWinPanelActivation;
+        private void OnDisable() => turnOrganizer.OnEndMatch += StartWinPanelActivation;
 
-        private void ActivateWinPanel(bool playerWon)
+        private void StartWinPanelActivation(bool playerWon) => StartCoroutine(ActivateWinPanel(playerWon));
+
+        private IEnumerator ActivateWinPanel(bool playerWon)
         {
+            yield return new WaitForSeconds(waitEndTime);
+
             endText.text = playerWon ? "You won!" : "You lost!";
             endPanel.SetActive(true);
         }
